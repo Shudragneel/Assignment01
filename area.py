146a4fef24ae349ce485f5296f1d70058647335c
Shudragneel <61274048+Shudragneel@users.noreply.github.com>
2020-03-09 11:09:56 +0530

    Update area.py

--- area.py
+++ area.py
@@ -8,5 +8,5 @@ Created on Thu Feb 20 13:40:24 2020
 #Write a  program to enter base and height of a rectangle and find its area.
 a=int(input("Enter the value for base:"))
 b=int(input("Enter the value for height:"))
-area = (a*b)/2
+area = (a*b)
 print(area)
